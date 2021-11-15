628d1ab4363cd61898d6f0a14af8bd6fdf699161
vccheng2001 <vccheng@andrew.cmu.edu>
2021-11-14 18:46:47 -0800

    recent captures page

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z78_!g8jm^kgq70o7d$v%&m#xk8azex^!ce%+%rykx2c5*doe7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
